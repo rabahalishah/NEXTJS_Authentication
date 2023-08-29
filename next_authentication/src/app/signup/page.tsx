6ea22453b51644ea22cea9f407c2f9a675695fcb
Rabah Ali Shah <muhammadrabah.ali46@gmail.com>
2023-08-29 06:44:21 +0500

    Formatted the docs and imported some libraries

--- next_authentication/src/app/signup/page.tsx
+++ next_authentication/src/app/signup/page.tsx
@@ -1,8 +1,12 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Axios } from "axios";
 export default function SignupPage() {
-    return (
-      <div className="text-center">
-        <h1 className="text-white text-2xl">Signup</h1>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="text-center">
+      <h1 className="text-white text-2xl">Signup</h1>
+    </div>
+  );
+}

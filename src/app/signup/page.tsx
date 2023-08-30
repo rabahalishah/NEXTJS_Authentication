c0bcfc11c5105581b28e1b7a27028fa356fc40f4
Rabah Ali Shah <muhammadrabah.ali46@gmail.com>
2023-08-30 15:30:33 +0500

    made some changes in the page.txs

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Axios } from "axios";
 export default function SignupPage() {
+  const [user, setUser] = useState({ email: "", password: "", usename: "" });
+
+  const onSignup = async () => {};
   return (
-    <div className="text-center">
-      <h1 className="text-white text-2xl">Signup</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Signup</h1>
+      <hr/>
+      <label htmlFor="username">username</label>
     </div>
   );
 }
